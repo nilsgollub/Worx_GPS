8e09601433f4f7fc4f71db783d1f8be1989cb7e6
nilsgollub <gollubnils@googlemail.com>
2024-06-09 13:52:40 +0200

    Aktualisieren von run.sh

--- worx_gps_tracker/run.sh
+++ worx_gps_tracker/run.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-# Umgebungsvariablen setzen
-export ENV_FILE=$(jq --raw-output '.env_file' /data/options.json)
-export OUTPUT_DIR=$(jq --raw-output '.output_dir' /data/options.json)
-
-# Skript ausführen
 python3 /app/Worx_GPS.py
