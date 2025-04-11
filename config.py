73935e3ff798a8fd590c8897a9187c42ef3f0f50
nilsgollub <gollubnils@googlemail.com>
2025-04-11 23:40:19 +0200

    fixed data_recorder.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ MQTT_CONFIG = {
 
 # GPS
 GEO_CONFIG = {
-    "is_fake": True,
+    #"is_fake": True,
     "fake_gps_range": ((46.811819, 46.811919), (7.132838, 7.132938)),  # (lat, lon)
     "lat_bounds": (46.810819, 46.812919),  # (min, max)
     "lon_bounds": (7.131838, 7.133938),  # (min, max)
@@ -45,7 +45,7 @@ REC_CONFIG = {
     "serial_port": os.getenv("GPS_SERIAL_PORT"),
     # Standardwert 9600 hinzugefügt, falls nicht in .env
     "baudrate": int(os.getenv("GPS_BAUDRATE", "9600")),
-    "test_mode": os.getenv("TEST_MODE", "False").upper() == "TRUE",
+    "test_mode": os.getenv("TEST_MODE", "False").upper() == "FALSE",
     # --- Fehlenden Schlüssel hinzufügen ---
     "storage_interval": 2  # Oder einen anderen Wert, falls gewünscht
     # --- Ende der Korrektur ---
