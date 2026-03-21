c792681e8db22f541ea6b1109efa7e9006627c65
nilsgollub <gollubnils@googlemail.com>
2026-03-21 22:42:14 +0100

    feat: Implement Flask web UI with SocketIO, serving React frontend and providing API endpoints for system data and configuration.

--- web_ui/webui.py
+++ web_ui/webui.py
@@ -129,8 +129,8 @@ system_monitor = None
 @app.route('/', defaults={'path': ''})
 
 @app.route('/<path:path>')
-
 def serve_react(path):
+    logger.debug(f"[ServeReact] Request Path: {path} | Method: {request.method}")
 
     """
 
